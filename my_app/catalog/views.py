fb9ea24694842cd144b769bec9714eb94edb3038
Shalabh Aggarwal <shalabh7777@gmail.com>
2014-09-23 13:53:12 +0530

    Chapter 3 - Recipe 'Creating a relational Category model'

--- my_app/catalog/views.py
+++ my_app/catalog/views.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify, Blueprint
 from my_app import db
-from my_app.catalog.models import Product
+from my_app.catalog.models import Product, Category
 
 catalog = Blueprint('catalog', __name__)
 
@@ -23,7 +23,8 @@ def products():
     for product in products:
         res[product.id] = {
             'name': product.name,
-            'price': str(product.price)
+            'price': product.price,
+            'category': product.category.name
         }
     return jsonify(res)
 
@@ -32,7 +33,37 @@ def products():
 def create_product():
     name = request.form.get('name')
     price = request.form.get('price')
-    product = Product(name, price)
+    categ_name = request.form.get('category')
+    category = Category.query.filter_by(name=categ_name).first()
+    if not category:
+        category = Category(categ_name)
+    product = Product(name, price, category)
     db.session.add(product)
     db.session.commit()
     return 'Product created.'
+
+
+@catalog.route('/category-create', methods=['POST',])
+def create_category():
+    name = request.form.get('name')
+    category = Category(name)
+    db.session.add(category)
+    db.session.commit()
+    return 'Category created.'
+
+
+@catalog.route('/categories')
+def categories():
+    categories = Category.query.all()
+    res = {}
+    for category in categories:
+        res[category.id] = {
+            'name': category.name
+        }
+        for product in category.products:
+            res[category.id]['products'] = {
+                'id': product.id,
+                'name': product.name,
+                'price': product.price
+            }
+    return jsonify(res)
