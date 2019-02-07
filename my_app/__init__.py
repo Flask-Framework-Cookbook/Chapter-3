3140f36a749a34ee573cc3c3bab6f868b7446bf9
Shalabh Aggarwal <shalabh7777@gmail.com>
2019-02-07 16:00:00 +0530

    Chapter 3 - Recipe 'Database migrations using Alembic and Flask-Migrate'

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 from my_app.catalog.views import catalog
 app.register_blueprint(catalog)
