a84eb6a625699344a75312ca946cc549f4342c8f
Shalabh Aggarwal <shalabh7777@gmail.com>
2019-02-07 16:27:25 +0530

    Chapter 3 - Recipe 'Opting the NoSQL way with MongoDB' Changes as per new Flask

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,19 +1,14 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, MigrateCommand
+from flask_mongoengine import MongoEngine
 from redis import Redis
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['MONGODB_SETTINGS'] = {'DB': 'my_catalog'}
 app.debug = True
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = MongoEngine(app)
 
 redis = Redis()
 
 from my_app.catalog.views import catalog
 app.register_blueprint(catalog)
-
-db.create_all()
