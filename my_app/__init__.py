ac787f780bcb3ca5f8c7ab7748dc116d48f2da45
Shalabh Aggarwal <shalabh7777@gmail.com>
2019-02-07 16:16:43 +0530

    Chapter 3 - Recipe 'Model data indexing with Redis' Make some changes as per Python3

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -2,13 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from redis import Redis
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.debug = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+redis = Redis()
 
 from my_app.catalog.views import catalog
 app.register_blueprint(catalog)
