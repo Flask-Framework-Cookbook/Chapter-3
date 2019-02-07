dfecfa45d8a95d8c1eb6213725cd5dcfee4b0d73
Shalabh Aggarwal <shalabh7777@gmail.com>
2019-02-07 15:42:00 +0530

    Fixes as per new changes in flask_sqlalchemy

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
